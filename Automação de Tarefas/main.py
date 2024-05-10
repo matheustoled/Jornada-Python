1229b8767fd719a9c8e83f88cfa800a51f08e482
matheustoled <matheustole304@gmail.com>
2024-05-10 00:57:22 -0300

    novo código de automação -> aprendizagem

--- Automação de Tarefas/main.py
+++ Automação de Tarefas/main.py
@@ -2,7 +2,7 @@ import pyautogui as pa
 import pandas as pd
 import time
 
-pa.PAUSE = 0.4
+pa.PAUSE = 0.5
 
 #entrar google
 pa.press("win")
@@ -12,7 +12,7 @@ pa.press("enter")
 #entrar site
 pa.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pa.press("enter")
-time.sleep(2)
+time.sleep(1)
 
 #logar
 pa.click(x=900, y=375)
@@ -26,8 +26,30 @@ time.sleep(1)
 tabela = pd.read_csv("produtos.csv")
 print(tabela)
 
+for i in tabela.index:
+    pa.click(x=950, y=260)
+    pa.hotkey("ctrl","a")
+    pa.press("backspace")
+    pa.write(str(tabela.loc[i, "codigo"]))
 
+    pa.press("tab")
+    pa.write(str(tabela.loc[i, "marca"]))
+    
+    pa.press("tab")
+    pa.write(str(tabela.loc[i, "tipo"]))
+    
+    pa.press("tab")
+    pa.write(str(tabela.loc[i, "categoria"]))
+    
+    pa.press("tab")
+    pa.write(str(tabela.loc[i, "preco_unitario"]))
+    
+    pa.press("tab")
+    pa.write(str(tabela.loc[i, "custo"]))
+    
+    pa.press("tab")
+    pa.write(str(tabela.loc[i, "obs"]))
 
-pa.click(x=940, y=260)
-pa.write()
-pa.press("tab")
+    pa.press("tab")
+    pa.press("enter")
+    time.sleep(0.6)
